ef81d704af9f091b9ce38938993f09d35551f80f
Sureshkumar <304624@cognizant.com>
2020-03-02 13:47:08 +0530

    to hold common functionalities

---
+++ common.js
@@ -0,0 +1,46 @@
+/* calculate number of days between */
+function getDaysBetween(startDate, endDate, getWeekDays) {
+   let daysBetween = 0;
+   return new Promise((resolve, _reject) => {
+      if (!startDate || isNaN(startDate) || !endDate || isNaN(endDate)) {
+         resolve(daysBetween);
+      } else {
+         /* clone date to avoid messing up original data */
+         let fromDate = new Date(startDate);
+         let toDate = new Date(endDate);
+
+         fromDate.setUTCHours(0, 0, 0, 0);
+         endDate.setUTCHours(0, 0, 0, 0);
+
+         if (fromDate.getTime() === toDate.getTime()) {
+            let dayOfWeek = fromDate.getDay();
+            if (getWeekDays === true) {
+               if (dayOfWeek > 0 && dayOfWeek < 6) {
+                  daysBetween++;
+               }
+            } else {
+               daysBetween++;
+            }
+            resolve(daysBetween);
+         } else {
+            while (fromDate <= toDate) {
+               fromDate.setDate(fromDate.getDate() + 1);
+               let dayOfWeek = fromDate.getDay();
+               /* check if the date is neither a Sunday(0) nor a Saturday(6) */
+               if (getWeekDays === true) {
+                  if (dayOfWeek > 0 && dayOfWeek < 6) {
+                     daysBetween++;
+                  }
+               } else {
+                  daysBetween++;
+               }
+            }
+            resolve(daysBetween);
+         }
+      }
+   });
+};
+
+module.exports = {
+   getDaysBetween
+}

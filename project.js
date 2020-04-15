4f1eb247e6b04f05070379e608f1de3b57a19028
prem88dev <premkumar88pro@gmail.com>
2020-04-15 09:27:07 +0530

    moved employee list to employee.js

--- project.js
+++ project.js
@@ -1,6 +1,5 @@
 const dbObj = require('./database');
 const empObj = require('./employee');
-const empProjColl = "emp_proj";
 const esaProjColl = "esa_proj";
 
 
@@ -30,64 +29,11 @@ function listAllProjects() {
 }
 
 
-/*
-   listEmployeeInProj:
-      to get list of employees in a project
-
-   params:
-      empEsaLink - linker between employee and project
-      ctsEmpId - employee id
-      revenueYear - year for which leaves are required
-      personal - boolean flag
-         if true, will fetch personal leaves
-         else, will fetch location holidays
-
-   returns an array with leave details
-*/
-function listEmployeeInProj(esaId) {
-   return new Promise((resolve, reject) => {
-      db = dbObj.getDb();
-      db.collection(empProjColl).aggregate([
-         {
-            $lookup: {
-               from: "esa_proj",
-               localField: "empEsaLink",
-               foreignField: "empEsaLink",
-               as: "empEsaProj"
-            }
-         },
-         {
-            $unwind: "$empEsaProj"
-         },
-         {
-            $match: {
-               "esaId": esaId
-            }
-         },
-         {
-            $group: {
-               "_id": "$_id",
-               "empFname": { "$first": "$empFname" },
-               "empMname": { "$first": "$empMname" },
-               "empLname": { "$first": "$empLname" }
-            }
-         }
-      ]).toArray(function (err, allProj) {
-         if (err) {
-            reject(err);
-         } else {
-            resolve(allProj);
-         }
-      });
-   });
-}
-
-
 function getProjectRevenue(esaId, revenueYear) {
    let allEmpRevArr = [];
    let revenueArr = [];
    return new Promise((resolve, _reject) => {
-      listEmployeeInProj(esaId).then((empInProj) => {
+      empObj.listEmployeeInProj(esaId).then((empInProj) => {
          empInProj.forEach((employee) => {
             let empObjId = employee._id.toString();
             allEmpRevArr.push(empObj.getEmployeeProjection(empObjId, revenueYear));
@@ -114,6 +60,5 @@ function getProjectRevenue(esaId, revenueYear) {
 
 module.exports = {
    listAllProjects,
-   listEmployeeInProj,
    getProjectRevenue
 }
